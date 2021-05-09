060f3bfab0f45ce59d201823f2960243b61e8793
example <alisayar0107@gmail.com>
2021-05-09 12:35:16 +0300

    constants created in the script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const playerScoreEl = document.getElementById('player-score');
+const playerChoiceEl = document.getElementById('player-choice');
+const computerChoiceEl = document.getElementById('computer-choice');
+const computerChoiceEl = document.getElementById('computer-choice');
+const resultText = document.getElementById('result-text');
+
+// constants for player icons
+const playerRock = document.getElementById('player-rock');
+const playerPaper = document.getElementById('player-paper');
+const playerScissors = document.getElementById('player-scissors');
+const playerLizard = document.getElementById('player-lizard');
+const playerSpock = document.getElementById('player-spock');
+
+// constants for computer icons
+const computerRock = document.getElementById('computer-rock');
+const computerPaper = document.getElementById('computer-paper');
+const computerScissors = document.getElementById('computer-scissors');
+const computerLizard = document.getElementById('computer-lizard');
+const computerSpock = document.getElementById('computer-spock');
+
 const choices = {
   rock: { name: 'Rock', defeats: ['scissors', 'lizard'] },
   paper: { name: 'Paper', defeats: ['rock', 'spock'] },
